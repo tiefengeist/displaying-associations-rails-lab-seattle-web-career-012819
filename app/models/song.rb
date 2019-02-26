b12c9b2092cc4b7e2555203f344524342cc88651
tiefengeist <sahirnambiar@gmail.com>
2019-02-26 10:07:07 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  def artist_name
+    self.artist.name
+  end
+  before_save :make_title_case
+
+   def make_title_case
+    self.title.titlecase
+  end
 end
